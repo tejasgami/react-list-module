73494cee9ec6c777577eba13405f3e846a718424
Tejas Gami <tejasgami111@gmail.com>
2019-05-03 01:20:56 +0530

    remove unused code

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,5 +2,5 @@ import { API_URL } from "./config";
 
 export const BASE_URL = API_URL() + "/api";
 
-// Users api urls
+// Users api urls CURRENTLY static 
 export const GET_USERS = () => "https://randomuser.me/api/";
